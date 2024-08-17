7ebda334c336326179fdee2843069e59e7edad16
frederick-hernandez <53789782+frederick-hernandez@users.noreply.github.com>
2024-08-17 17:15:47 -0600

    Add files via upload

---
+++ app/config/env.js
@@ -0,0 +1,17 @@
+
+
+const env = {
+  database: 'prueba_pzg6',
+  username: 'prueba_pzg6_user',
+  password: 'heE2GsfIO8KLxTcCDnjnYZWbK67CVvfT',
+  host: 'dpg-cqin3vij1k6c739hjh4prueba_pzg6g-a',
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
+
+module.exports = env;
